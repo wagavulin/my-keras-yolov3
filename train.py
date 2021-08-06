845a8d646d0b41834adfa3131485f538815210a3
Wataru Shimizu <waruzilla@gmail.com>
2021-08-07 00:35:17 +0900

    fix typo

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='../drive/MyDrive/peddeet/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='../drive/MyDrive/peddet/yolo_weights.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
