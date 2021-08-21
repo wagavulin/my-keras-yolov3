03404a31776c8ce8d9f73a2b59d1e24b4c528d20
Wataru Shimizu <waruzilla@gmail.com>
2021-08-21 20:36:39 +0900

    Save model file to google drive

--- train.py
+++ train.py
@@ -35,8 +35,8 @@ def _main():
             freeze_body=2, weights_path='../drive/MyDrive/peddet/darknet53_weights.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
-    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+    checkpoint = ModelCheckpoint("../drive/MyDrive/peddet/" + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+        monitor='val_loss', save_weights_only=True, save_best_only=False, period=3)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
